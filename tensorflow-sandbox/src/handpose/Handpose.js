5117de01f408599af963c0e281eb2e8b36ee9f94
Jena Boehm <boehmjena@gmail.com>
2021-02-23 17:34:33 -0800

    basic detect hands and logging detections

--- tensorflow-sandbox/src/handpose/Handpose.js
+++ tensorflow-sandbox/src/handpose/Handpose.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
 
 export default function Handpose() {
+    const webcamRef = useRef(null);
+    const canvasRef = useRef(null);
+
+    //load handpose model
+    const runHandpose = async() => {
+        const net = await handpose.load()
+        console.log('Handpose loaded');
+
+        //loop and detect hands
+        setInterval(() => {
+            detect(net)
+          }, 100)
+        
+    }
+
+    //detect function
+    const detect = async(net) => {
+        //check data is available
+        if(typeof webcamRef.current !== 'undefined' &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4) {
+        
+        //get video properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        //set video height and width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        //set canvas height and width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        //make detections
+        const hand = await net.estimateHands(video); 
+        console.log(hand);  
+
+        //draw mesh
+    }
+}
+
+    runHandpose();
+
     return (
         <div>
-            Handpose
+        <Webcam
+            ref={webcamRef}
+            style={{
+            //should be refactored into css file
+            position: 'absolute',
+            marginLeft:'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+            }}
+        />
+        <canvas
+            ref={canvasRef}
+            style={{
+            //same, refactor into css file
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+            }}
+        />
         </div>
     )
 }
